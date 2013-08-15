3e7e414af1cde1d79df2e2128f33a07c4c957600
Josh Teng <joshteng@webermize.com>
2013-08-16 02:08:18 +0800

    modify view

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -14,5 +14,21 @@ module Spree
       crumb_list = content_tag(:ul, raw(crumbs.flatten.map{|li| li.mb_chars}.join), class: 'inline')
       content_tag(:nav, crumb_list, id: 'breadcrumbs', class: 'span12')
     end
+
+    def link_to_cart(text = nil)
+      return "" if current_spree_page?(spree.cart_path)
+
+      text = text ? h(text) : Spree.t('cart')
+      css_class = nil
+      if current_order.nil? or current_order.line_items.empty?
+        text = "#{text} (0)"
+        css_class = 'empty'
+      else
+        text = "#{text}<!--span class='amount'>#{current_order.display_total.to_html}</span-->(#{current_order.item_count})".html_safe
+        css_class = 'full'
+      end
+
+      link_to text, spree.cart_path, :class => "cart-info #{css_class}"
+    end
   end
 end
